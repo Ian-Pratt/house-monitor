7f3c80ed863893b7d8dcdc103d1f849e102ac813
Ian Pratt <ian.pratt@gmail.com>
2021-03-13 13:17:40 +0000

    update

--- house_monitor.sh
+++ house_monitor.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 logger house_monitor
 
 cd /var/opt/data
 
-nohup /usr/bin/python3 /home/ian/pulse.py || logger pulse fail &
+/usr/bin/python3 /home/ian/pulse.py &>> pulse.out || logger pulse fail &
 
 logger next
 
-nohup /home/ian/go/src/github.com/nDenerserve/SmartPi/bin/smartpireadout > 3phase-`date --iso-8601=seconds`.log || logger smart fail &
+/home/ian/go/src/github.com/nDenerserve/SmartPi/bin/smartpireadout &>> smartpi.out || logger smart fail &
+
+logger discord
+
+/usr/bin/python3 /home/ian/discord/send.py &>> discord.out || logger discord fail &
+
 
 
 
